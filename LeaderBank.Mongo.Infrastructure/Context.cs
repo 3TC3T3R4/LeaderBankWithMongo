e6834340c099734a61e61160daa0c22c905b7a62
ESTEVAN TANGARIFE CORREA <teban1928@gmail.com>
2023-03-28 14:22:00 -0500

    Created Main Context

---
+++ LeaderBank.Mongo.Infrastructure/Context.cs
@@ -0,0 +1,30 @@
+﻿using LeaderBank.Mongo.Infrastructure.Entities;
+using LeaderBank.Mongo.Infrastructure.Interfaces;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeaderBank.Mongo.Infrastructure
+{
+    public class Context : IContext
+    {
+        private readonly IMongoDatabase _database;
+
+        public Context(string stringConnection, string DBname)
+        {
+            MongoClient cliente = new MongoClient(stringConnection);
+            _database = cliente.GetDatabase(DBname);
+        }
+
+        public IMongoCollection<CustomerEntity> Customer => _database.GetCollection<CustomerEntity>("customers");
+
+        public IMongoCollection<AccountEntity> Account => _database.GetCollection<AccountEntity>("accounts");
+
+        public IMongoCollection<CardEntity> Card => _database.GetCollection<CardEntity>("cards");
+
+        //public IMongoCollection<TransactionEnitity> Transaction => _database.GetCollection<TransactionEnitity>("transactions");
+    }
+}
