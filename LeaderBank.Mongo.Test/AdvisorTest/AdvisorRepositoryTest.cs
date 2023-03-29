a5594b8f77e0806d6d621a3ccfaf33acfd827d79
Rolando <rolandoantoniohe@ufps.edu.co>
2023-03-28 21:13:12 -0500

    Test
    test AdvisorRepository - Get and Post

---
+++ LeaderBank.Mongo.Test/AdvisorTest/AdvisorRepositoryTest.cs
@@ -0,0 +1,85 @@
+﻿using Castle.Core.Resource;
+using LeaderBank.Mongo.Domain.Entities;
+using LeaderBank.Mongo.Domain.UseCases.Gateway.Repositories;
+using LeaderBank.Mongo.Infrastructure.Entities;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeaderBank.Mongo.Test.AdvisorTest
+{
+    public class AdvisorRepositoryTest
+    {
+        private readonly Mock<IAdvisorRepository> _mockadvisorRepository;
+        public AdvisorRepositoryTest()
+        {
+            _mockadvisorRepository = new();
+        }
+
+        [Fact]
+        public async Task AddAdvisor()
+        {
+            // Arrange
+            var advisor = new Advisor
+            {
+                Names = "rola",
+                SurNames = "Test",
+                Address = "Test",
+                Email = "rola@mail.com",
+                Phone = "123456789",
+                Birthdate = DateTime.Now,
+                Gender = "F"
+
+            };
+            var advisorEntity = new AdvisorEntity
+            {
+                Advisor_Id = Guid.NewGuid().ToString(),
+                Names = "rola",
+                Surnames = "Test",
+                Address = "Test",
+                Email = "rola@mail,com",
+                Phone = "123456789",
+                Birthdate = DateTime.Now,
+                Gender = "F"
+
+            };
+            _mockadvisorRepository.Setup(x => x.AddAdvisor(advisor)).ReturnsAsync(advisor);
+            // Act
+            var result = await _mockadvisorRepository.Object.AddAdvisor(advisor);
+            // Assert
+            Assert.NotNull(result);
+            Assert.Equal(advisorEntity.Names, result.Names);
+          
+        }
+
+        [Fact]
+        public async Task GetListAdvisors()
+        {
+            // Arrange
+            var advisor = new Advisor
+            {
+                Names = "rola",
+                SurNames = "Test",
+                Address = "Test",
+                Email = "rola@mail.com",
+                Phone = "123456789",
+                Birthdate = DateTime.Now,
+                Gender = "F"
+            };
+
+            var advisorList = new List<Advisor> { advisor };
+            _mockadvisorRepository.Setup(x => x.GetListAdvisors()).ReturnsAsync(advisorList);
+
+            //Act
+            var result = await _mockadvisorRepository.Object.GetListAdvisors();
+
+            //Assert
+            Assert.NotNull(result);
+            Assert.Equal(advisorList, result);
+        }
+        
+    }
+}
---
+++ LeaderBank.Mongo.Test/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace LeaderBank.Mongo.Test
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
